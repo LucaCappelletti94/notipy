2e510eff9ea784528e6cc148181fcf907a8c95ac
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-06-16 22:01:36 +0200

    Wrapped new version

--- notipy_me/notipy_me.py
+++ notipy_me/notipy_me.py
@@ -11,126 +11,93 @@ from typing import List, Dict, Callable, Tuple
 import humanize
 import time
 import pandas as pd
-from validate_email import validate_email
 from validators import domain
 from environments_utils import is_stdout_enabled
 from tabulate import tabulate
+from validate_email import validate_email
 import sys
 from traceback import format_tb
+from userinput import userinput, set_validator, start
+
 
 class Notipy(ContextDecorator):
-    _config_path = ".notipy.json"
-
     def __init__(self):
         """Create a new istance of Notipy."""
         super(Notipy, self).__init__()
-        self._enabled = is_stdout_enabled()
-        if self._enabled:
-            self._load_cache()
-            print("Let's setup your notipy!")
-            print("Hit enter to use the default values.")
-            self._config["email"] = self._get("email", self._validate_email)
-            self._password = getpass.getpass("Password: ")
-            self._config["task_name"] = self._get("task_name")
-            if "recipients" not in self._config:
-                self._config["recipients"] = self._config["email"]
-            self._config["recipients"] = self._get(
-                "recipients",
-                validator=self._validate_emails,
-                comment=", separated by a comma")
-            timeouts = {
-                "h":24,
-                "m":30,
-                "s":120
-            }
-            if "report_timeout_unit" not in self._config:
-                self._config["report_timeout_unit"] = "h"
-            self._config["report_timeout_unit"] = self._get(
-                "report_timeout_unit",
-                validator=lambda x: x in timeouts,
-                comment=", {timeouts}".format(timeouts=tuple(timeouts.keys())))
-            if "report_timeout" not in self._config:
-                self._config["report_timeout"] = timeouts[self._config["report_timeout_unit"]]
-            unit = {
-                "h":"hours",
-                "m":"minutes",
-                "s":"seconds"
-            }[self._config["report_timeout_unit"]]
-            self._config["report_timeout"] = int(self._get(
-                "report_timeout",
-                validator=self._positive_int,
-                comment=", in {unit}".format(unit=unit)))
-            if "port" not in self._config:
-                self._config["port"] = 465
-            self._config["port"] = int(self._get(
-                "port",
-                validator=self._positive_int))
-            if "smtp_server" not in self._config:
-                self._config["smtp_server"] = "smtp.{server}".format(server=".".join(
-                    self._config["email"].split("@")[1].split(".")[-2:])
-                )
-            self._config["smtp_server"] = self._get(
-                "smtp_server",
-                validator=self._validate_server)
-            self._store_cache()
-            self._report = self._interrupt_txt = self._interrupt_html = None
+        self._enabled = False
+        self._setup()
+        self._report = self._interrupt_txt = self._interrupt_html = None
 
-    def _get(self, parameter: str, validator: Callable = None, comment: str = "", default: str = ""):
-        default = " [{parameter}]".format(
-            parameter=self._config[parameter]
-        ) if parameter in self._config else ""
-        while True:
-            user_input = input("Please insert {parameter}{comment}{default}: ".format(
-                default=default,
-                parameter=parameter,
-                comment=comment)).strip()
-            if not user_input and default and (validator is None or validator(self._config[parameter])):
-                return self._config[parameter]
-            if validator is None or validator(user_input):
-                return user_input
-            print("The given {parameter} '{user_input}' is not valid.".format(
-                parameter=parameter, user_input=user_input))
+    @start
+    def _setup(self):
+        self._enabled = True
+        print("Let's setup your notipy!")
+        print("Hit enter to use the default values.")
+        self._always_use_default = userinput("always_use_default",
+                                     label="Should I always use the defaults?",
+                                     default="no",
+                                     sanitizer="human_bool",
+                                     validator=set_validator(["yes", "no"]))
+        self._email = userinput("email", validator="email", always_use_default=self._always_use_default)
+        self._password = getpass.getpass("Password: ")
+        self._send_start_email = userinput("start_email",
+                                     label="Should I send a start email too?",
+                                     default="yes",
+                                     sanitizer="human_bool",
+                                     validator=set_validator(["yes", "no"]))
+        self._task_name = userinput("task name", validator="non_empty", always_use_default=self._always_use_default)
+        self._recipients = userinput("recipients", default=self._email,
+                                     label="Please insert {name}, separated by a comma",
+                                     validator=lambda x: all([
+                                         validate_email(email) for email in x.split(",")
+                                     ]),
+                                     always_use_default=self._always_use_default
+                                     ).split(",")
+        timeouts = {
+            "hours": 24,
+            "minutes": 30,
+            "seconds": 120
+        }
+        self._report_timeout_unit = userinput(
+            "report_timeout_unit",
+            label="Please insert {{name}}, choosing from {choices}".format(
+                choices=", ".join(timeouts.keys())
+            ),
+            default="hours",
+            validator=set_validator(timeouts.keys()),
+            always_use_default=self._always_use_default
+        )
+        self._report_timeout = int(userinput(
+            "report_timeout",
+            label="Please insert {{name}} in {unit}".format(
+                unit=self._report_timeout_unit
+            ),
+            default=timeouts[self._report_timeout_unit],
+            validator="positive_integer",
+            always_use_default=self._always_use_default
+        ))
+        self._port = int(userinput(
+            "port",
+            default=465,
+            validator="positive_integer",
+            always_use_default=self._always_use_default
+        ))
 
-    def _validate_email(self, email:str):
-        return not email.endswith("gmail.com") and validate_email(email)        
-
-    def _validate_emails(self, emails: str):
-        return all([
-            validate_email(email) for email in emails.split(",")
-        ])
-
-    def _validate_server(self, server: str):
-        return domain(server) and self._is_server_reacheable(server)
-
-    def _is_server_reacheable(self, server: str)->bool:
-        try:
-            socket.gethostbyname(server)
-            return True
-        except socket.gaierror:
-            return False
-
-    def _positive_int(self, value: str):
-        return int(value) > 0
-
-    def _load_cache(self):
-        if os.path.exists(self._config_path):
-            with open(self._config_path, "r") as f:
-                self._config = json.load(f)
-        else:
-            self._config = {}
-
-    def _store_cache(self):
-        with open(self._config_path, "w") as f:
-            json.dump(self._config, f, indent=4)
+        self._smtp_server = userinput(
+            "smtp_server",
+            default="smtp.{server}".format(server=".".join(
+                self._email.split("@")[1].split(".")[-2:])),
+            validator="hostname",
+            always_use_default=self._always_use_default
+        )
 
     def _notify(self, subject: str, txt: str, html: str):
-        server_ssl = SMTP_SSL(
-            self._config["smtp_server"], self._config["port"])
-        server_ssl.login(self._config["email"], self._password)
+        server_ssl = SMTP_SSL(self._smtp_server, self._port)
+        server_ssl.login(self._email, self._password)
         msg = MIMEMultipart('alternative')
         msg["Subject"] = subject
-        msg["To"] = self._config["recipients"]
-        msg["From"] = self._config["email"]
+        msg["To"] = ",".join(self._recipients)
+        msg["From"] = self._email
         msg.attach(MIMEText(txt, 'plain'))
         msg.attach(MIMEText(html, 'html'))
         server_ssl.sendmail(msg["From"], msg["To"], msg.as_string())
@@ -181,7 +148,8 @@ class Notipy(ContextDecorator):
             "interrupt_html": "" if self._interrupt_html is None else self._interrupt_html,
             "report_html": "" if self._report is None else self._report.tail().to_html(),
             "report_txt": "" if self._report is None else tabulate(self._report.tail(), tablefmt="pipe", headers="keys"),
-            **self._config
+            "email": self._email,
+            "task_name": self._task_name
         }
 
     def _build_models(self, name: str)->Tuple[str, str, str]:
@@ -208,11 +176,11 @@ class Notipy(ContextDecorator):
         self._report = df if self._report is None else pd.concat([
             self._report, df
         ])
-        if time.time() - self._last_report > self._config["report_timeout"]*{
-            "h":60*60,
-            "m":60,
-            "s":1
-        }[self._config["report_timeout_unit"]]:
+        if time.time() - self._last_report > self._report_timeout*{
+            "hours": 60*60,
+            "minutes": 60,
+            "seconds": 1
+        }[self._report_timeout_unit]:
             self._last_report = time.time()
             self._send_report()
 
@@ -220,7 +188,8 @@ class Notipy(ContextDecorator):
         if self._enabled:
             self._start_time = time.time()
             self._last_report = time.time()
-            self._start()
+            if self._send_start_email:
+                self._start()
         return self
 
     def __exit__(self, exc_type, exc_val, traceback):
--- tests/test_notipy.py
+++ tests/test_notipy.py
@@ -1,14 +1,15 @@
 from notipy_me import Notipy
 from time import sleep
 import pandas as pd
+import pytest
 
 def test_notipy_me():
-    try:
+    with pytest.raises(ValueError):
         with Notipy() as nm:
             print("Running test")
             sleep(3)
             nm.add_report(pd.DataFrame({"small":[12,3,5]}))
             sleep(2)
             raise ValueError("Test")
-    except ValueError:
-        pass
+    with Notipy() as nm:
+        sleep(1)
